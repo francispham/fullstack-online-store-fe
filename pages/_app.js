dd1cad52df31d0e723c24a1c97506b260d9418fa
Francis <francispham89@gmail.com>
2020-07-11 20:41:48 -0700

    Styled Components: Themes, Layout, Typography, & Global Styling with injectGlobal

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import App, { Container } from 'next/app';
+
 import Page from '../components/Page'
+
 class MyApp extends App {
   render () {
     const { Component } = this.props;
 
     return (
       <Container>
-        <p>Hey I am the App, I'm on Every Page</p>
         <Page>
           <Component />
         </Page>
