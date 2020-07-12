e3bb1e63af7b5bfce2c771ce839eb66f504ce731
Francis <francispham89@gmail.com>
2020-07-11 21:54:15 -0700

    Implemented Visualizing Route Changes Feature & Fixed Syntax Errors in Meta.js

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,22 @@
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import styled from 'styled-components';
 
 import Nav from './Nav';
 
+/* Routing */ 
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
+
+/* Styled Components */ 
 const Logo = styled.h1`
   font-size: 4rem;
   margin-left: 2rem;
@@ -40,6 +54,7 @@ const StyledHeader = styled.header`
     border-bottom: 1px solid ${props => props.theme.lightgrey};
   }
 `;
+
 
 const Header = () => (
   <StyledHeader>
