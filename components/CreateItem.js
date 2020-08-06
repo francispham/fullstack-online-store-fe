8060771f3c8f78a0f8cec954f6976acd8e7fee05
Francis <francispham89@gmail.com>
2020-08-05 20:49:37 -0700

    Implementing GraphQL Mutations for Creating Items: Created & Called Mutation to enable Form Functionality

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION (
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem (
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
 
 export default class CreateItem extends Component {
   state = {
@@ -20,51 +44,63 @@ export default class CreateItem extends Component {
 
   render() {
     return (
-      <Form onSubmit={e => {
-        e.preventDefault();
-        console.log(this.state);
-      }}>
-        <fieldset>
-          <label htmlFor="title">
-            Title
-            <input 
-              value={this.state.title}
-              placeholder="Title" 
-              name="title" 
-              type="text" 
-              id="title" 
-              required
-              onChange={this.handleChange}
-            />
-          </label>
+      <Mutation 
+        mutation={CREATE_ITEM_MUTATION}
+        variables={this.state}
+      >
+        {(createItem, { loading, error }) => (
+          <Form onSubmit={ async e => {
+            e.preventDefault();
+            // Call the Mutation
+            const res = await createItem();
+            console.log('res:', res)
+          }}>
+            <Error error={error} />
+            <fieldset disable={loading} aria-busy={loading}>
+              <label htmlFor="title">
+                Title
+                <input 
+                  value={this.state.title}
+                  placeholder="Title" 
+                  name="title" 
+                  type="text" 
+                  id="title" 
+                  required
+                  onChange={this.handleChange}
+                />
+              </label>
 
-          <label htmlFor="price">
-            Price
-            <input 
-              value={this.state.price}
-              placeholder="Price" 
-              name="price" 
-              type="number" 
-              id="price" 
-              required
-              onChange={this.handleChange}
-            />
-          </label>
+              <label htmlFor="price">
+                Price
+                <input 
+                  value={this.state.price}
+                  placeholder="Price" 
+                  name="price" 
+                  type="number" 
+                  id="price" 
+                  required
+                  onChange={this.handleChange}
+                />
+              </label>
 
-          <label htmlFor="description">
-            Description
-            <textarea 
-              value={this.state.description}
-              placeholder="Enter A Description" 
-              name="description" 
-              id="description" 
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit">Submit</button>
-        </fieldset>
-      </Form>
+              <label htmlFor="description">
+                Description
+                <textarea 
+                  value={this.state.description}
+                  placeholder="Enter A Description" 
+                  name="description" 
+                  id="description" 
+                  required
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     )
   }
 };
+
+export { CREATE_ITEM_MUTATION };
