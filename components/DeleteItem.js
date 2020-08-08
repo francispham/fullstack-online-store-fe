5b63f073875aec17d2e1037a66cb013157d65094
Francis <francispham89@gmail.com>
2020-08-08 16:22:00 -0700

    Implementing GraphQL Queries & Mutation for Deleting Items: Updated List on Page with Query using Cache & Payload after Deleting an Item

---
+++ components/DeleteItem.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { ALL_ITEMS_QUERY } from './Items';
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // Manually Update the Cache on the Client, so it matches the Server
+    // 1. Read the Cache for the Items wanted
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log('data:', data)
+    // 2. Filter the Deleted Item out of the Page
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    );
+    // 3. Put the Remain Items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data }) 
+  };
+  
+  render() {
+    return (
+      <Mutation 
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update= {this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button onClick={() => {
+            if (confirm('Are You Sure You Want to DELETE this Item?')) {
+              deleteItem();
+            }
+          }}>
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
+    );
+  };
+};
--- components/Item.js
+++ components/Item.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
 import Title from './styles/Title';
-import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import ItemStyles from './styles/ItemStyles';
 import formatMoney from '../lib/formatMoney';
+
+import DeleteItem from './DeleteItem';
 
 export default class Item extends Component {
   static propTypes = {
@@ -14,15 +16,17 @@ export default class Item extends Component {
 
   render() {
     const { item } = this.props;
-    
     return (
       <ItemStyles>
         {item.image && <img src={item.image} alt={item.title} />}
+
         <Title>
-          <Link href={{
-            pathname: '/item',
-            query: { id: item.id },
-          }}>
+          <Link
+            href={{
+              pathname: '/item',
+              query: { id: item.id },
+            }}
+          >
             <a>{item.title}</a>
           </Link>
         </Title>
@@ -30,16 +34,18 @@ export default class Item extends Component {
         <p>{item.description}</p>
 
         <div className="buttonList">
-          <Link href={{
-            pathname: 'update',
-            query: { id: item.id },
-          }}>
-            <a>Edit</a>
+          <Link
+            href={{
+              pathname: 'update',
+              query: { id: item.id },
+            }}
+          >
+            <a>Edit ✏️</a>
           </Link>
-          <button>Add to Cart</button>
-          <button>Delete</button>
+          <button>Add To Cart</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
-  };
-};
+  }
+}
