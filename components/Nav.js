b7cf2f3d4a281889aa017e392f2c3dd06188a0e4
Francis <francispham89@gmail.com>
2020-08-14 15:38:56 -0700

    User Signout & Completed Navbar Authentication Flow

--- components/Nav.js
+++ components/Nav.js
@@ -1,33 +1,37 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+
 import User from './User';
+import Signout from './Signout';
 
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {/* Second Layer Data Destructuring */}
-      {({ data: { me } }) => {
-        console.log('me:', me);
-        if (!me) return null;
-        return <p>Current User</p>;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me ? (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+            <Signout />
+          </>
+        ) : (
+          <Link href="/signup">
+            <a>Signup</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
