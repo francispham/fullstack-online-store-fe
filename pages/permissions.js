eba0efa1ffbaa362e5a1bef60ac5580b0f980687
Francis <francispham89@gmail.com>
2020-08-22 13:10:49 -0700

    Authorization Flow in Permissions Management: Created Permissions Page

---
+++ pages/permissions.js
@@ -0,0 +1,13 @@
+import PleaseSignin from '../components/PleaseSignin';
+
+const Permissions = props => {
+  return (
+    <div>
+      <PleaseSignin>
+        <p>Permissions!</p>
+      </PleaseSignin>
+    </div>
+  );
+};
+
+export default Permissions;
