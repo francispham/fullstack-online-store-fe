36bf156e93f822fb20e0647a262d4e397645498c
Francis <francispham89@gmail.com>
2020-08-29 13:32:58 -0700

    Shopping Cart Feature-Displaying Items: Imported Data from User Component into Cart Component

--- components/Cart.js
+++ components/Cart.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
 
-import CartStyles from './styles/CartStyles';
+import User from './User';
+
 import Supreme from './styles/Supreme';
-import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
+import CartStyles from './styles/CartStyles';
+import CloseButton from './styles/CloseButton';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -20,26 +22,37 @@ const TOGGLE_CART_MUTATION = gql`
 `;
 
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>
-    {toggleCart => (
-      <Query query={LOCAL_STATE_QUERY}>
-        {({data}) =>
-          <CartStyles open={data.cartOpen}>
-            <header>
-              <CloseButton onClick={toggleCart} title="close">&times;</CloseButton>
-              <Supreme>Your Cart</Supreme>
-              <p>You Have __ Items in your cart.</p>
-            </header>
+  <User>
+    {({data: { me }}) => {
+      if(!me) return null;
+      console.log('me:', me);
+      return (
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+          {toggleCart => (
+            <Query query={LOCAL_STATE_QUERY}>
+              {({data}) =>
+                <CartStyles open={data.cartOpen}>
+                  <header>
+                    <CloseButton onClick={toggleCart} title="close">&times;</CloseButton>
+                    <Supreme>{me.name}'s Cart</Supreme>
+                    <p>
+                      You Have {me.cart.length} Item
+                      {me.cart.length === 1 ? '' : 's'} in your cart.
+                    </p>
+                  </header>
+                  <footer>
+                    <p>$10.00</p>
+                    <SickButton>Checkout</SickButton>
+                  </footer>
+                </CartStyles>
+              }
+            </Query>
+          )}
+        </Mutation>
+      );
+    }}
+  </User>
 
-            <footer>
-              <p>$10.00</p>
-              <SickButton>Checkout</SickButton>
-            </footer>
-          </CartStyles>
-        }
-      </Query>
-    )}
-  </Mutation>
 );
 
 export default Cart;
