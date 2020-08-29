889abe3e8cd4c6fc821f38e4c0eea22fbc5dd442
Francis <francispham89@gmail.com>
2020-08-29 11:33:25 -0700

    Shopping Cart Feature-Adding Items: Implemented Backend Integration with React Apollo Mutation for Adding Items to Cart

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation addToCart($id: ID!) {
+    addToCart(id: $id) {
+      id
+      quantity
+    }
+  }
+`;
 
 class AddToCart extends React.Component {
   render() {
-    const { id } = this.props;
-    return <button>Add This To Cart</button>;
+    const { id } = this.props;    
+    return (
+      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
+        {addToCart =><button onClick={addToCart}>Add This To Cart</button>}
+      </Mutation>
+    );
   };
 };
 
