e0c25a800ced4b5116e0e671c66fc5bdefc9c335
Francis <francispham89@gmail.com>
2020-08-30 21:11:27 -0700

    Shopping Cart Feature-Deleting Items: Removed Items from Cart

--- components/CartItem.js
+++ components/CartItem.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import formatMoney from '../lib/formatMoney';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
   display: grid;
@@ -31,6 +32,7 @@ const CartItem = ({ cartItem }) => (
         </em>
       </p>
     </div>
+    <RemoveFromCart id={cartItem.id} />
   </CartItemStyles>
 );
 
