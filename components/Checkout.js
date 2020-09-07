c8155c7d4687ae5b55d6df711796ce216561ed1d
Francis <francispham89@gmail.com>
2020-09-07 00:22:18 -0700

    Credit Card Checkout Feature: Created createOrder Mutation & Implemented token Property from StripeCHeckout to Execute createOrder with token receipted

--- components/Checkout.js
+++ components/Checkout.js
@@ -1,27 +1,64 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 import StripeCheckout from 'react-stripe-checkout';
 
-import User from './User';
+import User, { CURRENT_USER_QUERY } from './User';
 import calcTotalPrice from '../lib/calcTotalPrice';
 
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 };
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 class Checkout extends React.Component {
+  onToken = (res, createOrder) => {
+    console.log('res:', res);
+    // Manually Call the Mutation once we have the Stripe Token
+    createOrder({
+      variables: {
+        token: res.id
+      },
+    }).catch(err => {
+      alert(err.message);
+    });
+  };
+
   render() {
     return (
       <User>
         {({ data: { me } }) => (
-          <StripeCheckout
-            amount={calcTotalPrice(me.cart)}
-            name="Next Store"
-            description={`Order of ${totalItems(me.cart)} items!`}
-            image={me.cart[0].item && me.cart[0].item.image} // Check & Display Image
-            stripeKey="pk_test_powbTm7SuxCdQafatiw61KQm"
-            currency="CAN"
-            email={me.email}
-          >{this.props.children}</StripeCheckout>
+          <Mutation 
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries= {[{ query: CURRENT_USER_QUERY }]}
+          >
+            {createOrder => (
+              <StripeCheckout
+                amount={calcTotalPrice(me.cart)}
+                name="Next Store"
+                description={`Order of ${totalItems(me.cart)} items!`}
+                image={me.cart[0].item && me.cart[0].item.image} // Check & Display Image
+                stripeKey="pk_test_powbTm7SuxCdQafatiw61KQm"
+                email={me.email}
+                currency="CAD"
+                token={res => this.onToken(res, createOrder)}
+              >{this.props.children}</StripeCheckout>
+            )}
+          </Mutation>
+          
         )}
       </User>
     )
