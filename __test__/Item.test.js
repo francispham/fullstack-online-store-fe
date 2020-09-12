5fbbd36390fb31e31d305a28ba322c4eebf434ed
Francis <francispham89@gmail.com>
2020-09-12 12:16:56 -0700

    Testing-Unit Test-Shallow Rendering: Implemented Testing Item Component with Shallow API's Methods

--- __test__/Item.test.js
+++ __test__/Item.test.js
@@ -16,6 +16,14 @@ describe('<Item />', () => {
   it('renders and displays properly', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem}/>);
     // 'shallow' just render the top level component, not the true HTML
-    console.log('wrapper:', wrapper.debug());
+    // console.log('wrapper:', wrapper.debug());
+
+    const PriceTag = wrapper.find('PriceTag');
+    console.log('PriceTag:', PriceTag.children());
+    expect(PriceTag.children().text()).toBe('$500');
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+    const ImageTag = wrapper.find('img');
+    expect(ImageTag.props().src).toBe(fakeItem.image);
+    expect(ImageTag.props().alt).toBe(fakeItem.title);
   })
 })
