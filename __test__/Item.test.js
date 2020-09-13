b8db8109225a772131846659ebf1561ec8359dbf
Francis <francispham89@gmail.com>
2020-09-12 19:46:47 -0700

    Testing-Unit Test-Shallow Rendering: Implemented & Divided the Tests by their Purposes

--- __test__/Item.test.js
+++ __test__/Item.test.js
@@ -13,17 +13,35 @@ const fakeItem = {
 };
 
 describe('<Item />', () => {
-  it('renders and displays properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem}/>);
-    // 'shallow' just render the top level component, not the true HTML
-    // console.log('wrapper:', wrapper.debug());
-
+  /*
+    'shallow' API just render the top level component, not the true HTML.
+    Use 'debug' Method to find out which item to be tested.
+  */ 
+  const wrapper = shallow(<ItemComponent item={fakeItem}/>);
+  // console.log('wrapper:', wrapper.debug());
+  
+  it('renders price tags & titles properly', () => {
     const PriceTag = wrapper.find('PriceTag');
-    console.log('PriceTag:', PriceTag.children());
+    // console.log('PriceTag:', PriceTag.children());
     expect(PriceTag.children().text()).toBe('$500');
+    
     expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  });
+  
+  it('renders the images properly', () => {
     const ImageTag = wrapper.find('img');
     expect(ImageTag.props().src).toBe(fakeItem.image);
     expect(ImageTag.props().alt).toBe(fakeItem.title);
-  })
-})
+  });
+  
+  it('renders out the buttons properly', () => {
+    const ButtonList = wrapper.find('.buttonList');
+    expect(ButtonList.children()).toHaveLength(3);
+
+    console.log('ButtonList:', ButtonList.debug());
+    expect(ButtonList.find('Link')).toHaveLength(1); // Or
+    expect(ButtonList.find('Link').exists()).toBe(true);
+
+    expect(ButtonList.find('DeleteItem').exists()).toBe(true);
+  });
+});
